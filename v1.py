02eedc8b5f2eb298adf5823552902b9d63510c9d
gael-lycee <gael.muller.lycee@gmail.com>
2026-01-06 17:04:09 +0100

    Update v1.py

--- v1.py
+++ v1.py
@@ -1,5 +1,10 @@
 from time import *
 
+def get(truc):
+  if truc == "temperature":
+    temperature = #il faut definir
+  if truc == "luminositee":
+    luminositee = #il faut definir
 def on_IF():
   #il faut definir
 def off_IF():
@@ -10,11 +15,11 @@ def open_door():
   #il faut definir
 
 while True:
-  temprature = #il faut definir comment chercher la temperature
+  get(temperature)
   if temperature < 10:
     on_IF()
     close_door()
   elif temperature > 20:
     off_IF()
     open_door()
-    
+  get(luminositee)
