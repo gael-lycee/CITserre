9a12dc84ee762303de70467b836939a57c481fab
gael-lycee <gael.muller.lycee@gmail.com>
2026-01-06 17:00:58 +0100

    Update v1.py

--- v1.py
+++ v1.py
@@ -1,1 +1,20 @@
+from time import *
 
+def on_IF():
+  #il faut definir
+def off_IF():
+  #il faut definir
+def close_door():
+  #il faut definir
+def open_door():
+  #il faut definir
+
+while True:
+  temprature = #il faut definir comment chercher la temperature
+  if temperature < 10:
+    on_IF()
+    close_door()
+  elif temperature > 20:
+    off_IF()
+    open_door()
+    
